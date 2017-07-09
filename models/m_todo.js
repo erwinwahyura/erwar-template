fced087535921ab79d544b6d46e5f9f3bcddd888
erwinwahyura <you@example.com>
2017-07-09 23:50:57 +0700

    add collections

---
+++ models/m_todo.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var todoSchema = new Schema({
+  title:  {
+    type : String,
+    required : [true, 'title is required! please fill it!']
+  },
+  description: {
+    type : String,
+    required : [true, 'description is required! please fill it!']
+  },
+  createdAt: {
+    type : String
+  },
+  completed: {
+    type : Boolean,
+    require : [true, 'completed is required! please fill it!']
+  },
+  creator: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+var todo = mongoose.model('Todo', todoSchema);
+
+module.exports = todo
