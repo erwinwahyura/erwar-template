1ad14e29e46863e8137c71d9465a1b3ad2257f92
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-07-17 19:42:48 +0700

    add environment

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 //you can change database by yourself by typing your db name in below after mongodb://localhost/your_database_name
-mongoose.connect('mongodb://localhost/erwar-todoFancy');
-
-//this is setting when you are using some third party for online db, for the example im using mlab
-// mongoose.connect('mongodb://<erwinwahyura>:<dbpassword>@ds121222.mlab.com:21222/lab-hactiv8-overflow');
+mongoose.connect('mongodb://erwinwahyura:'+process.env.yourpassword+'@ds161042.mlab.com:61042/erwar-todo')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
